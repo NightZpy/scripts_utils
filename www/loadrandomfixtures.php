21acecd3bb7a8fa9f9117ad967826704666a6a04
NightZpy <nightzpy@gmail.com>
2013-03-13 16:42:09 -0430

    Agregando nuevas opciones para la carga de los datos automaticos

--- www/loadrandomfixtures.php
+++ www/loadrandomfixtures.php
@@ -11,7 +11,7 @@ class LoadRandomFixtures extends Conexion
 		$this->text = $text;
 	}
 
-	public function load($table = '')
+	public function load($table = '', $count)
 	{
 		$colums = $this->getColumnsForTable($table);
 		$cf = 0;
@@ -30,7 +30,7 @@ class LoadRandomFixtures extends Conexion
 			}
 			else {
 				$strFields .= ', '.$fieldName;
-				$strValues .= ', '.$this->getDataFromType($fieldType, $fieldLen, $extra);
+				$strValues .= ', '.$this->getDataFromType($fieldType, $fieldLen, $extra, $table, $count);
 			}
 			$cf++;
 		}
@@ -44,17 +44,17 @@ class LoadRandomFixtures extends Conexion
 		return preg_replace("/([^0-9])/", "", $type); 
 	}
 
-	public function getDataFromType($type = '', $size = 0, $extra = '')
+	public function getDataFromType($type = '', $size = 0, $extra = '', $table = '', $count = 0)
 	{
 		//$strData = '';
 		//echo "<br /><b>Tipo: $type</b>";
 		if (strpos($type, 'varchar') !== FALSE || strpos($type, 'char') !== FALSE) {
-			return "'".substr($this->text, 0, $size)."'";
+			return "'".substr(substr($table, 0, $size - 1) + $count, 0)."'";
+			//return "'".substr($this->text, 0, $size)."'";
 		} elseif (strpos($type, 'tinytext') !== FALSE || strpos($type, 'tinyblob') !== FALSE || 
 				  strpos($type, 'text') !== FALSE || strpos($type, 'blob') !== FALSE ||
 				  strpos($type, 'mediumtext') !== FALSE || strpos($type, 'mediumblob') !== FALSE ||
-				  strpos($type, 'longtext') !== FALSE || strpos($type, 'longblob') !== FALSE) {
-			
+				  strpos($type, 'longtext') !== FALSE || strpos($type, 'longblob') !== FALSE) {			
 			return "'".substr($this->text, 0, 512)."'";
 		} elseif (strpos($type, 'tinyint') !== FALSE || strpos($type, 'smallint') !== FALSE || 
 				  strpos($type, 'mediumint') !== FALSE || strpos($type, 'integer') !== FALSE || 
@@ -87,6 +87,10 @@ class LoadRandomFixtures extends Conexion
 	}
 }
 
+?>
+<h1>Cargando Base Datos: <?php echo $db; ?></h1>
+<?php
+
 $text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae feugiat lacus. Nam euismod quam vitae ligula pretium ac malesuada mauris mollis. Fusce ornare sollicitudin aliquam. Sed mauris nisi, eleifend vitae iaculis id, ultricies sed metus. Proin sit amet velit vitae est pellentesque tristique ut eget lectus. Phasellus molestie rhoncus nisl et sodales. Maecenas elit velit, feugiat eget blandit at, sagittis vel velit. Curabitur ut sem vitae arcu gravida facilisis et ut erat. Aenean gravida pretium nisi eu mollis. Curabitur nisi ante, tempor et convallis eget, gravida nec eros. Nam placerat euismod sapien, ac consectetur diam consequat ut.
 Mauris eleifend molestie leo pellentesque pharetra. Curabitur sem quam, adipiscing quis condimentum id, porttitor id lectus. Aliquam erat volutpat. Aliquam placerat dignissim erat id semper. Aliquam erat volutpat. In augue justo, elementum ut tincidunt id, lobortis eu elit. Aliquam dui lacus, molestie id hendrerit non, tincidunt quis felis. Nunc tempus sem aliquet eros egestas vitae lacinia dolor egestas.
 Aenean suscipit, felis quis pharetra aliquet, urna massa facilisis augue, vel lacinia enim risus eget magna. Phasellus rhoncus lacinia purus, non dignissim lorem iaculis ac. Praesent vitae nibh nunc. Etiam eget hendrerit mauris. Donec orci felis, auctor ut bibendum eget, lacinia a nisi. Aenean lorem velit, posuere eu ultrices ut, faucibus a tellus. Donec elit felis, pulvinar at tincidunt nec, vehicula porta libero. Nulla gravida erat id arcu mollis fermentum. Aliquam posuere tempus metus, at lacinia mi fermentum quis. Quisque rutrum ultricies magna, lacinia bibendum enim cursus nec. Proin libero neque, vestibulum ac ornare pulvinar, posuere id purus.
@@ -96,21 +100,23 @@ Proin urna nibh, euismod vel rutrum in, ultricies at tortor. Phasellus vestibulu
 Aenean pharetra consequat ligula, sed ullamcorper tellus porta vel. Morbi cursus lacus at nulla elementum commodo. Suspendisse potenti. Ut venenatis augue eget nisl laoreet feugiat. Mauris ornare sem eget felis ultricies vestibulum at vitae magna. Nunc id massa velit. Sed augue dolor, imperdiet ut tincidunt eget, sagittis et ligula. Fusce auctor orci quis lectus euismod fermentum. Quisque vel diam leo. Nam eget nibh ligula, cursus tempus nisi.
 Mauris nisl dui, iaculis vestibulum congue id, pellentesque vel enim. Integer consequat lobortis pharetra. Curabitur molestie sollicitudin mollis. Praesent vel nisl sapien. Etiam dictum justo euismod enim auctor lobortis. Nam imperdiet elit ut lectus facilisis tempor. Nullam erat metus, volutpat a fermentum ut, viverra id diam. Nam viverra felis in eros sagittis tincidunt. Sed eleifend elit in diam egestas fringilla. Curabitur a ante in velit facilisis mollis.
 Suspendisse posuere consequat leo sed vestibulum. In hac habitasse platea dictumst. Aliquam ornare cursus lacus, a elementum nisi rhoncus nec. Mauris mattis augue vitae arcu ullamcorper in fringilla elit porta. Pellentesque ac lectus non elit dignissim facilisis. Sed et auctor tortor. Nunc vel facilisis orci. Nulla facilisi. Donec commodo nullam.';
-?>
-<h1>Cargando Base Datos: <?php echo $db; ?></h1>
-<?php
 
-$db = 'blog';
-$conexion = new Conexion('root', '18990567', $db, 'localhost');
-$tablas = $conexion->getTablesName();
-$load = new LoadRandomFixtures('root', '18990567', $db, 'localhost', $text);
+$db = 'mercosur';
+//$conexion = new Conexion('bashman', '123456', $db, 'mercosur.localhost');
+//$tablas = $conexion->getTablesName();
+$tablas = array('paises', 'estados', 'ramos', 'rubros', 'tipo_instituciones', 'localidades', 'direcciones', 'empresas',
+				'personas', 'mesas', 'horarios', 'encuentros', 'productos', 'demandas', 'ofertas',
+				'reuniones', 'ofertantes', 'demandantes', 'empresas_rubros');
+
+
+$load = new LoadRandomFixtures('bashman', '123456', $db, 'mercosur.localhost', $text);
 $nRegs = 10;
 foreach ($tablas as $tabla){
 ?>
 	<h2>Cargando Tabla: <?php echo $tabla; ?></h2>
 <?php
-	for ($i=0; $i < $nRegs; $i++)
-		if($conexion->agregarRegistro($load->load($tabla))) echo "<br />Agregado!";
+	for ($i=1; $i < $nRegs; $i++)
+		if($conexion->agregarRegistro($load->load($tabla), $i)) echo "<br />Agregado!";
 }
 ?>
 
